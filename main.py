bd7eb1997d52cb5e00becabd3098a686bea53aaa
Tenebris <naniiloveminecraft@outlook.com>
2023-08-03 11:28:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import json
 def check_password(plaintext_password, hashed_password):
     return crypt.crypt(plaintext_password, hashed_password) == hashed_password
 
+canvas_list = []
+
 url = 'http://example.com/path/to/remote_file.json'
 response = requests.get(url)
 root = ctk.CTk()
@@ -17,29 +19,77 @@ root.minsize(640, 480)
 root.maxsize(640, 480)
 root.title("Login System Alpha")
 
-def login():
-    log_inButton.destroy()
-    sign_upButton.destroy()
-    label = ctk.CTkLabel(root, text="Log Into Nexum Alpha",
-                         fg_color="transparent", font=('Calibri Light', 17))
+def destroy_all_canvases():
+    for canvas in canvas_list:
+        canvas.destroy()
+    canvas_list.clear()
+
+def signup(event):
+    destroy_all_canvases()
+    signuppagecanvas = ctk.CTkCanvas(root, background="black")
+    signuppagecanvas.pack(fill=ctk.BOTH, expand=True)
+    back.lift(signuppagecanvas)
+    canvas_list.append(signuppagecanvas)
+    label = ctk.CTkLabel(signuppagecanvas, text="Sign up to Nexum Alpha",
+                         fg_color="transparent", font=('Calibri Light', 17), text_color='white')
     label.place(x=230, y=80)
-    uslabel = ctk.CTkLabel(root, text="Username :",
-                           fg_color="transparent", font=('arial', 15))
+    uslabel = ctk.CTkLabel(signuppagecanvas, text="Username :",
+                           fg_color="transparent", font=('arial', 15), text_color='white')
     uslabel.place(x=210, y=110)
-    usentry = ctk.CTkEntry(master=root, width=300, font=("arial", 17))
-    usentry.place(x=180, y=140)
-    pwlabel = ctk.CTkLabel(root, text="Password :",
-                           fg_color="transparent", font=('arial', 15))
+    us_rentry = ctk.CTkEntry(master=signuppagecanvas, width=300, font=("arial", 17), text_color='white', fg_color='gray')
+    us_rentry.place(x=180, y=140)
+    pwlabel = ctk.CTkLabel(signuppagecanvas, text="Password :",
+                           fg_color="transparent", font=('arial', 15), text_color='white')
     pwlabel.place(x=210, y=170)
-    pwentry = ctk.CTkEntry(master=root, width=300, font=("arial", 17), show="*")
+    pw_entry = ctk.CTkEntry(master=signuppagecanvas, width=300, font=("arial", 17), show="*", text_color='white', fg_color='gray')
     bool_var = ctk.BooleanVar()
     bool_button = ctk.CTkCheckBox(
-        root, text="Network Login", variable=bool_var)
+        root, text="Network Registering", variable=bool_var, bg_color='black', text_color='white')
+    bool_button.place(x=380, y=240)
+    pw_entry.place(x=180, y=200)
+
+    def on_register_button_click(event):
+        password = pw_entry.get()
+        username = us_rentry.get()
+        register(username, password)
+
+    register_Button = ctk.CTkButton(signuppagecanvas, text="Register")
+    register_Button.place(x=220, y=240)
+    register_Button.bind("<Button-1>", on_register_button_click)
+
+def getback(event=None):
+    destroy_all_canvases()
+    maincanvas.pack(fill=ctk.BOTH, expand=True)
+
+    #back.bind("<Button-1>", getback)
+
+
+def login():
+    maincanvas.destroy()
+    loginpagecanvas = ctk.CTkCanvas(root, background="black")
+    loginpagecanvas.pack(fill=ctk.BOTH, expand=True)
+    back.lift(loginpagecanvas)
+    canvas_list.append(loginpagecanvas)
+    label = ctk.CTkLabel(loginpagecanvas, text="Log Into Nexum Alpha",
+                         fg_color="transparent", font=('Calibri Light', 17), text_color='white')
+    label.place(x=230, y=80)
+    uslabel = ctk.CTkLabel(loginpagecanvas, text="Username :",
+                           fg_color="transparent", font=('arial', 15), text_color='white')
+    uslabel.place(x=210, y=110)
+    usentry = ctk.CTkEntry(master=loginpagecanvas, width=300, font=("arial", 17), text_color='white', fg_color='gray')
+    usentry.place(x=180, y=140)
+    pwlabel = ctk.CTkLabel(loginpagecanvas, text="Password :",
+                           fg_color="transparent", font=('arial', 15), text_color='white')
+    pwlabel.place(x=210, y=170)
+    pwentry = ctk.CTkEntry(master=loginpagecanvas, width=300, font=("arial", 17), show="*", text_color='white', fg_color='gray')
+    bool_var = ctk.BooleanVar()
+    bool_button = ctk.CTkCheckBox(
+        loginpagecanvas, text="Network Login", variable=bool_var, bg_color='black', text_color='white')
     bool_button.place(x=380, y=240)
     pwentry.place(x=180, y=200)
 
     # Use lambda to pass the function with arguments without executing it immediately
-    loginButton = ctk.CTkButton(root, text="Log In", command=lambda: log_in_local(username=usentry.get(), password=pwentry.get()))
+    loginButton = ctk.CTkButton(loginpagecanvas, text="Log In", command=lambda: log_in_local(username=usentry.get(), password=pwentry.get(), loginpagecanvas=loginpagecanvas))
     loginButton.place(x=220, y=240)
 
 # ... (remaining code
@@ -92,7 +142,6 @@ def log_in_local(username, password):
         return False
 # endregion
 
-# region sign up
 
 
 def register(username, password):
@@ -107,72 +156,27 @@ def register(username, password):
     }
 
     try:
-        with open('data/accounts/accounts.json', 'w') as json_file:
+        with open('data/accounts/accounts.json', 'r') as json_file:
             existing_data = json.load(json_file)
             messagebox.showinfo('Registered as', username)
     except FileNotFoundError:
-        # If the file doesn't exist yet, set existing_data to an empty list or dictionary
-        existing_data = []
+        messagebox.showinfo('An internal error has occured :(')
     existing_data.append()
     with open('data/accounts/accounts.json', 'w') as json_file:
         json.dump(existing_data, json_file, indent=4)
 
 
-def signup(event):
 
-    log_inButton.destroy()
-    sign_upButton.destroy()
-    label = ctk.CTkLabel(root, text="Sign up to Nexum Alpha",
-                         fg_color="transparent", font=('Calibri Light', 17))
-    label.place(x=230, y=80)
-    uslabel = ctk.CTkLabel(root, text="Username :",
-                           fg_color="transparent", font=('arial', 15))
-    uslabel.place(x=210, y=110)
-    us_rentry = ctk.CTkEntry(master=root, width=300, font=("arial", 17))
-    us_rentry.place(x=180, y=140)
-    pwlabel = ctk.CTkLabel(root, text="Password :",
-                           fg_color="transparent", font=('arial', 15))
-    pwlabel.place(x=210, y=170)
-    pw_entry = ctk.CTkEntry(master=root, width=300, font=("arial", 17), show="*")
-    bool_var = ctk.BooleanVar()
-    bool_button = ctk.CTkCheckBox(
-        root, text="Network Registering", variable=bool_var)
-    bool_button.place(x=380, y=240)
-    pw_entry.place(x=180, y=200)
-
-    def on_register_button_click(event):
-        password = pw_entry.get()
-        username = us_rentry.get()
-        register(username, password)
-
-    register_Button = ctk.CTkButton(root, text="Register")
-    register_Button.place(x=220, y=240)
-    register_Button.bind("<Button-1>", on_register_button_click)
-
-    def getback(event=None):
-        register_Button.destroy()
-        us_rentry.destroy()
-        pw_entry.destroy()
-        label.destroy()
-        pwlabel.destroy()
-        uslabel.destroy()
-
-        log_inButton = ctk.CTkButton(root, text="Log In", command=login)
-        log_inButton.place(x=330, y=200)
-        sign_upButton = ctk.CTkButton(root, text="Sign Up")
-        sign_upButton.bind("<Button-1>", signup)
-        sign_upButton.place(x=170, y=200)
-
-    back = ctk.CTkButton(root, text="back", command=getback)
-    back.place(x=20, y=20)
-    back.bind("<Button-1>", getback)
-
-# endregion
-
-
-log_inButton = ctk.CTkButton(root, text="Log In", command=login)
+#back.bind("<Button-1>", getback)
+maincanvas = ctk.CTkCanvas(root, background="black")
+maincanvas.pack(fill=ctk.BOTH, expand=True)
+canvas_list.append(maincanvas)
+back = ctk.CTkButton(root, text="back", command=getback)
+back.place(x=20, y=20)
+back.lift(maincanvas)
+log_inButton = ctk.CTkButton(maincanvas, text="Log In", command=login)
 log_inButton.place(x=330, y=200)
-sign_upButton = ctk.CTkButton(root, text="Sign Up")
+sign_upButton = ctk.CTkButton(maincanvas, text="Sign Up")
 sign_upButton.bind("<Button-1>", signup)
 sign_upButton.place(x=170, y=200)
 
